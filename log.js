5a0140fc506cfdba21ce4f9754f90a96ad4a3e7f
snaku001 <snaku001@fiu.edu>
2016-09-28 19:22:23 -0400

    Add files via upload

---
+++ log.js
@@ -0,0 +1,29 @@
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
+
+var LogSchema = new Schema({
+	projectid: {type: String, required: false},
+	student: {type: String, required: true},
+	firstName: {type: String, required: false},
+	lastName: {type: String, required: false},
+	fullName: {type: String, required: false},
+	studentemail: {type: String, required: true},
+	selectProject: {type: String, required: false},
+	description: {type: String, required: false},
+	image: {type: String, required: false},
+	term: { type: String, default: 1 },
+	minStudents: { type: Number, required: false},
+	maxStudents: { type: Number, required: false}, //End of Rev Project Proposal fields
+    gender : { type: String, required: false},	
+	department : { type: String, required: false},	
+	college : { type: String, required: false},	
+	major : { type: String, required: false},	//End of Rev Student Application fields
+	action: {type: String, required: true}, //Accept or Reject
+    type: {type: String, required: true}, //Ex: Review Project Proposal or Review Student Application
+    time: {type: Date, default: Date.now},
+    // User Story #1144
+    skillItem: {type: String, required: false}
+	
+});
+
+module.exports = mongoose.model('Log', LogSchema);
