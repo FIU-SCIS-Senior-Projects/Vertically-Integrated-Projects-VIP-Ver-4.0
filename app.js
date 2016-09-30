2c61cdf698d1f954c9d2345f8afa19c04be59dca
ragar005 <ragar005@fiu.edu>
2016-09-29 21:31:57 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+angular.module('mainApp', [
+	'textAngular',
+	'vipHeader',
+	'vipFooter',
+    'vip-projects',		//Features Module
+	'ProjectProposalController',
+	'reviewProfile',
+	'routes',
+	'projectApplicationController',
+	'userRegistrationController',
+	'forgotPasswordController',
+	'forgotPasswordService',
+	'contactController',
+	'userService',
+    'toDoModule',
+	'user-profile',
+	'reviewRegistration',
+	'folder',
+	'reviewStudentApp',
+	'reviewProjectProposals',
+	'admin',
+    'MessengerController',
+    // User Story #1142
+    'angularTrix'
+]).filter('selectedTags', function() {
+    return function(users, members)
+    {
+         if (users && members)
+         {
+             for(i = 0; i < users.length; i++) 
+             {
+                 //alert("i see " + users[i].email);
+                 //alert("comparing " + users[i].email);
+                 if (!members.includes(users[i].email))
+                 {
+                    //alert("found " + users[i].email);
+                    users.splice(i,1);
+                 }
+             }
+             return users;
+         }
+    };
+});
